b7b927607e10f16a2174e02128571c683a70c0f5
Liampobob <liam@thamlock.com>
2019-11-08 20:39:03 +0000

    retrieve basic api.github.com information on my user

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
+const id = "d75456e5099e1a296f5d"
+const secret = "009d78ce03d34d40a189850b9eb197bb12f2c9d8"
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.sendFile('./public/main.html', { root: __dirname });
-});
+app.get('/callback', (req, res) => {
+    const token = req.query.code;
+    axios({
+        method: 'post',
+        url: `https://github.com/login/oauth/access_token?client_id=${id}&client_secret=${secret}&code=${token}`,
+        headers: {
+             accept: 'application/json'
+        }
+
+    }).then((response) => {
+    
+        const accessToken = response.data.access_token
+        console.log(response.data)
+        
+        // redirect the user to the home page, along with the access token
+        res.redirect(`/home.html?access_token=${accessToken}`)
+    })
+})
 
 app.listen(8000)
